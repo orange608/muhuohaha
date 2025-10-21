dc7216e6601bcd42fbc06a249e8327ef1795a853
orange608 <3481098424@qq.com>
2025-10-21 17:40:18 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -1,9 +1,9 @@
-//
-//
-//
-#include <stdio.h>
+#include<stdio.h>
 int main()
 {
-    printf("Hello World");
+    float a = 0;
+    printf("请输入你的摄氏温度：");
+    scanf("%f", &a);
+    printf("你的华氏温度为3.1%f", a*9/5+32);
     return 0;
 }
