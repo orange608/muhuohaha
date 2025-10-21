39ff440067ef9380dcd8b44bf42a09a743b382e3
orange608 <3481098424@qq.com>
2025-10-21 19:08:04 +0800

    Create 3_main.c

---
+++ 3_main.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+	int a, b = 0;
+	char op = 0;
+	scanf("%d,%d,%c", &a,&b,&op);
+	switch (op) {
+	case '+':
+		printf("%d\n", a + b);
+		break;
+	case '-':
+		printf("%d\n", a - b);
+		break;
+	case '*':
+		printf("%d\n", a * b);
+		break;
+	case '/':
+		if (b != 0) {
+			printf("%.2f\n",(float)a / b);
+		}
+		else {
+			printf("除数不能为0\n");
+		}
+		break;
+	default:
+		printf("无效的运算符");
+	}
+	return 0;
+}
