e9257f643bfd429703a81a822d652f930990f099
orange608 <3481098424@qq.com>
2025-10-21 18:04:03 +0800

    Create 2_main.c

---
+++ 2_main.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{	
+	int a = 0;
+	printf("请输入成绩");
+	scanf("%d",&a);
+	if (a>=90&&a<=100) {
+		printf("A");
+	}
+	else if (a>=80&&a<=89) {
+		printf("B");
+	}
+	else if (a>=70&&a<=79) {
+		printf("C");
+	}
+	else if (a>=60&&a<=69) {
+		printf("D");
+	}
+	else if (a<60) {
+		printf("E");
+	}
+	return 0;
+}
