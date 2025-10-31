e34301941bb3a211def9e8448aeb74a82980901b
orange608 <3481098424@qq.com>
2025-10-31 23:08:12 +0800

    Update 3_main.c

--- 3_main.c
+++ 3_main.c
@@ -1,32 +1,35 @@
 //202510116208
 //3481098424@qq.com
 //吴纯宇
-#include<stdio.h>
-int main()
+#include <stdio.h>
+int main() 
 {
-	int a, b = 0;
-	char op = 0;
-	scanf("%d,%d,%c", &a,&b,&op);
-	switch (op) {
-	case '+':
-		printf("%d\n", a + b);
-		break;
-	case '-':
-		printf("%d\n", a - b);
-		break;
-	case '*':
-		printf("%d\n", a * b);
-		break;
-	case '/':
-		if (b != 0) {
-			printf("%.2f\n",(float)a / b);
-		}
-		else {
-			printf("除数不能为0\n");
-		}
-		break;
-	default:
-		printf("无效的运算符");
-	}
-	return 0;
+    int n = 0;
+    scanf("%d", &n);
+    if (n == 1) 
+    {
+        printf("密钥不安全，请重新输入");
+        return 0;
+    }
+    int i = 2;
+    int is_prime = 1;
+    while (i < n) 
+    {
+        if (n % i == 0) 
+        { 
+            is_prime = 0;
+            break;
+        }
+        i++;
+    }
+    if (is_prime) 
+    {
+        printf("密钥安全，密码设置成功");
+    }
+    else 
+    {
+        printf("密钥不安全，请重新输入");
+    }
+
+    return 0;
 }
