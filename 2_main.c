fdba4b0e44c78a9b3d5a93c9999f80777d802854
orange608 <3481098424@qq.com>
2025-10-31 22:58:39 +0800

    Update 2_main.c

--- 2_main.c
+++ 2_main.c
@@ -1,26 +1,31 @@
 //202510116208
 //3481098424@qq.com
 //吴纯宇
-#include<stdio.h>
+#include <stdio.h>
 int main()
-{	
-	int a = 0;
-	printf("请输入成绩");
-	scanf("%d",&a);
-	if (a>=90&&a<=100) {
-		printf("A");
-	}
-	else if (a>=80&&a<=89) {
-		printf("B");
-	}
-	else if (a>=70&&a<=79) {
-		printf("C");
-	}
-	else if (a>=60&&a<=69) {
-		printf("D");
-	}
-	else if (a<60) {
-		printf("E");
-	}
-	return 0;
+{
+    int num; 
+    int first = 1;
+    for (num = 100; num <= 999; num++) 
+	{
+        int bai = num / 100;
+        int shi = (num / 10) % 10;
+        int ge = num % 10;
+        int sum = bai * bai * bai +
+            shi * shi * shi +
+            ge * ge * ge;
+        if (sum == num)
+		{
+            if (first)
+			{
+                printf("%d", num);
+                first = 0;
+            }
+            else 
+			{
+                printf(" %d", num);
+            }
+        }
+    }
+    return 0;
 }
