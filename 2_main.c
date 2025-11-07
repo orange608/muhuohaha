5f6f89ae0757a20e8886abdcf646c8d6cb95da75
orange608 <3481098424@qq.com>
2025-11-07 21:48:25 +0800

    Update 2_main.c

--- 2_main.c
+++ 2_main.c
@@ -2,30 +2,27 @@
 //3481098424@qq.com
 //吴纯宇
 #include <stdio.h>
-int main()
-{
-    int num; 
-    int first = 1;
-    for (num = 100; num <= 999; num++) 
-	{
-        int bai = num / 100;
-        int shi = (num / 10) % 10;
-        int ge = num % 10;
-        int sum = bai * bai * bai +
-            shi * shi * shi +
-            ge * ge * ge;
-        if (sum == num)
-		{
-            if (first)
-			{
-                printf("%d", num);
-                first = 0;
-            }
-            else 
-			{
-                printf(" %d", num);
-            }
+int main() {
+    int arr[3][3];
+    int carr[3][3];
+    int i, j;
+    printf("请输入3×3矩阵的9个元素（每行3个，空格分隔）：\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            scanf("%d", &arr[i][j]);
         }
+    }
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            carr[j][i] = arr[i][j];
+        }
+    }
+    printf("\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            printf("%d ", carr[i][j]);
+        }
+        printf("\n");
     }
     return 0;
 }
