921ab08eac73363acbb1c130456f4066ae2ea1a3
orange608 <3481098424@qq.com>
2025-11-08 14:24:11 +0800

    Update 5_main.c

--- 5_main.c
+++ 5_main.c
@@ -1,22 +1,14 @@
+//202510116208
+//3481098424@qq.com
+//吴纯宇
 #include <stdio.h>
-int main() 
-{
-    int arr[5];
-    int i;
-    for (i = 0; i < 4; i++) 
-    {
-        scanf("%d", &arr[i]);
-    }
-    arr[4] = arr[0] + arr[1] + arr[2] + arr[3];
-    for (i = 0; i < 5; i++) 
-    {
-        if (i == 4) {
-            printf("%d", arr[i]);
-        }
-        else 
-        {
-            printf("%d ", arr[i]);
-        }
-    }
+int h(int a1, int an, int step) {
+    int n = (an - a1) / step + 1;
+    return (a1 + an) * n / 2;
+}
+
+int main() {
+    int sum = h(1, 100, 1);
+    printf("%d\n", sum);
     return 0;
 }
