7eb1ba874e5f0ee8913feb4de3340bf23147413d
orange608 <3481098424@qq.com>
2025-11-08 14:13:41 +0800

    Update 4_main.c

--- 4_main.c
+++ 4_main.c
@@ -2,31 +2,15 @@
 //3481098424@qq.com
 //吴纯宇
 #include <stdio.h>
-int main()
-{
-    int num = 100;
-    int first = 1;
-    while (num <= 999)
-    {
-        int bai = num / 100;
-        int shi = (num / 10) % 10;
-        int ge = num % 10;
-        int sum = bai * bai * bai +
-            shi * shi * shi +
-            ge * ge * ge;
-        if (sum == num)
-        {
-            if (first) 
-            {
-                printf("%d", num);
-                first = 0;
-            }
-            else
-            {
-                printf(" %d", num);
-            }
-        }
-        num++;
+int h(int a, int b) {
+    int result = 1;
+    for (int i = 0; i < b; i++) {
+        result *= a;
     }
+    return result;
+}
+int main() {
+    int sum = h(1, 2) + h(2, 2) +h(3, 2) + h(4, 2) + h(5, 2);
+    printf("%d\n", sum);
     return 0;
 }
